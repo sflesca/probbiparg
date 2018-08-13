73bb4f583b6506f4673ac4362d52b37a0aaef478
franco7scala <franco7scala@gmail.com>
2018-08-13 08:23:37 +0200

    written skeleton algorithm

---
+++ BipolarArgumentation/src/test/TestImprovedPrBAF.java
@@ -0,0 +1,114 @@
+package test;
+
+
+import java.util.List;
+
+import structures.PrBAF;
+
+
+public class TestImprovedPrBAF {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Generating sample graph...");
+		PrBAF baf = new PrBAF();
+		baf.addArg("a", 1);
+		baf.addArg("b", 1);
+		baf.addArg("c", 1);
+		baf.addArg("d", 1);
+		baf.addArg("e", 0.5);
+		baf.addArg("f", 1);
+		baf.addDefeat("e", "b", 0.5);
+		baf.addDefeat("b", "c", 1);
+		baf.addDefeat("f", "d", 1);
+		baf.addSupport("a", "b", 1);
+		baf.addSupport("c", "d", 1);
+		
+		Object S = null; //TODO
+		System.out.println("Calculating...");
+		long t = System.currentTimeMillis();
+		
+		
+		float Pr = 0.0f;
+		List<Object> A_e = computeAe();  
+		List<Object> R_e = computeRe();
+		for ( Object currentA_e : A_e ) {
+			for ( Object currentR_e : R_e ) {
+				float Pr_s = 0.0f;
+				if ( true ) { //TODO
+					Object F_p = contract(baf, currentA_e, currentR_e);
+					float Pr_p = 0; //TODO
+					Object F_s = complete(F_p, currentA_e, currentR_e);
+					if ( true ) { //TODO
+						if ( !isSafe(F_s, S, currentA_e, currentR_e) ) {
+							Pr_s = 0.0f;
+						}
+					}
+					else if ( true ) { //TODO
+						if ( !superClosed(F_s, S) ) {
+							Pr_s = 0.0f;
+						}
+					}
+					else {
+						Pr_s = 1.0f;
+					}
+					Object F_c = toPrAAF(F_s);
+					Object aafsem;
+					if ( true ) { //TODO
+						aafsem = null; //TODO
+					}
+					else {
+						aafsem = null; //TODO
+					}
+					float Pr_pp = computePrAAF(F_c, aafsem);
+					Pr = Pr + Pr_p * Pr_s * Pr_pp;
+				}
+			}
+		}
+		long elapsedTime = System.currentTimeMillis() - t;
+		System.out.println("Result: " + Pr);
+		System.out.println("Computation Time: " + elapsedTime);
+	}
+
+	private static float computePrAAF(Object f_c, Object aafsem) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private static Object toPrAAF(Object f_s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static boolean superClosed(Object f_s, Object s) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static boolean isSafe(Object f_s, Object s, Object currentA_e, Object currentR_e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static Object complete(Object f_p, Object currentA_e, Object currentR_e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object contract(PrBAF baf, Object currentAe, Object currentRe) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static List<Object> computeAe() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private static List<Object> computeRe() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
