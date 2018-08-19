ced1af5861a1a608ae9c66c0e20965d0374cc08d
Sergio Flesca <sergio.flesca@gmail.com>
2018-08-19 17:54:15 +0200

    generator

---
+++ BipolarArgumentation/src/generator/PrBAFGenerator.java
@@ -0,0 +1,92 @@
+package generator;
+
+import structures.ArgSet;
+import structures.PrBAF;
+import java.io.*;
+import java.util.Scanner;
+
+public class PrBAFGenerator {
+
+	static PrBAF generate(int numarg, double percatt, double percsup) {
+		PrBAF prbaf = new PrBAF();
+
+		for (int i = 0; i < numarg; i++)
+			prbaf.addArg("a" + i, Math.random());
+
+		for (int i = 0; i < numarg; i++)
+			for (int j = 0; j < numarg; j++)
+				if (Math.random() <= percatt)
+					prbaf.addDefeat("a" + i, "a" + j, Math.random());
+		
+		for (int i = 0; i < numarg; i++)
+			for (int j = 0; j < numarg; j++)
+				if (Math.random() <= percsup)
+					prbaf.addSupport("a" + i, "a" + j, Math.random());
+		return prbaf;
+	}
+
+	static public void printPrBAF(PrBAF prbaf, PrintStream f) {
+		f.println(prbaf.args.size());
+		for (String a : prbaf.args) {
+			f.println(a + " " + prbaf.argProb.get(a));
+		}
+		int numdefeats = 0;
+		for (String a : prbaf.defeats.keySet())
+			for (String c : prbaf.defeats.get(a)) {
+				numdefeats++;
+			}
+		f.println(numdefeats);
+		for (String a : prbaf.defeats.keySet()) {
+			for (String c : prbaf.defeats.get(a)) {
+				f.println(a + " " + c + " " + prbaf.defProb.get(a).get(c));
+			}
+		}
+		int numsupports = 0;
+		for (String a : prbaf.supports.keySet())
+			for (String c : prbaf.supports.get(a)) {
+				numsupports++;
+			}
+		f.println(numsupports);
+		for (String a : prbaf.supports.keySet()) {
+			for (String c : prbaf.supports.get(a)) {
+				f.println( a + " " + c + " " + prbaf.supProb.get(a).get(c));
+			}
+		}
+	}
+	
+	static public PrBAF readPrBA(InputStream f) {
+		PrBAF prbaf = new PrBAF();
+		Scanner sc = new Scanner(System.in);
+		int numarg = sc.nextInt();
+		for (int i = 0; i < numarg; i++) {
+			String arg = sc.next();
+			String sprob = sc.next();
+			double prob = Float.parseFloat(sprob);
+			prbaf.addArg(arg, prob);
+		}
+		int numdef = sc.nextInt();
+		for (int i = 0; i < numdef; i++) {
+			String arg1 = sc.next();
+			String arg2 = sc.next();
+			String sprob = sc.next();
+			double prob = Float.parseFloat(sprob);		
+			prbaf.addDefeat(arg1, arg2, prob);
+		}
+		int numsup = sc.nextInt();
+		for (int i = 0; i < numsup; i++) {
+			String arg1 = sc.next();
+			String arg2 = sc.next();
+			String sprob = sc.next();
+			double prob = Float.parseFloat(sprob);		
+			prbaf.addSupport(arg1, arg2, prob);
+		}
+		return prbaf;
+	}
+	
+	public static void main(String[] args) {
+		//PrBAF prbaf = generate(5,.2,.05);
+		PrBAF prbaf = readPrBA(System.in);
+		printPrBAF(prbaf, System.out);
+	}
+
+}
